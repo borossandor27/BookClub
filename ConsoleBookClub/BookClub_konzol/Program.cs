9f2894cf01d2e14df425b2c0644bb9bd2353d58a
borossandor27 <boros.sanyi@gmail.com>
2022-04-11 19:27:08 +0200

    2.0

---
+++ ConsoleBookClub/BookClub_konzol/Program.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+
+namespace BookClub_konzol
+{
+    class Program
+    {
+        static List<Tag> tagok = new List<Tag>();
+        static void Main(string[] args)
+        {
+            Beolvas();
+            befizetesek();
+            regebbi();
+            nemek();
+            Console.WriteLine("\nProgram vége!");
+            Console.ReadLine();
+        }
+
+        private static void nemek()
+        {
+            foreach (var item in tagok.GroupBy(a => a.Nem).Select(b => new {nem=b.Key, fo=b.Count() }))
+            {
+                Console.WriteLine($"\t{item.nem}\t{item.fo} fő");
+            }
+        }
+
+        private static void regebbi()
+        {
+            //-- Legrégebbi dolgozó
+            DateTime legelso = tagok.Min(a => a.Belepett);
+            Tag tag = tagok.Find(a => a.Belepett == legelso);
+            Console.WriteLine($"{tag.Id} {tag.Nev} ({tag.Belepett.ToString("yyyy-MM-dd")})");
+        }
+
+        private static void befizetesek()
+        {
+            //-- Az egyes tagok befizetései
+            foreach (Tag item in tagok)
+            {
+                int osszeg = item.befizetes.Sum(a => a.Osszeg);
+                Console.WriteLine($"\t{item.Nev}:\t{osszeg.ToString("#,##0")} Ft");
+            }
+        }
+
+        private static void Beolvas()
+        {
+            //-- adatok beolvasása az adatbázisból
+            //-- paraméterek beállítása
+            MySqlConnectionStringBuilder sb = new MySqlConnectionStringBuilder();
+            sb.Server = "localhost";
+            sb.UserID = "root";
+            sb.Password = "";
+            sb.Database = "bookclub";
+            MySqlConnection connection = new MySqlConnection(sb.ConnectionString);
+            try
+            {
+                connection.Open();
+                MySqlCommand sql = connection.CreateCommand();
+                sql.CommandText = "SELECT `id`, `csaladnev`, `utonev`, `nem`, `szuletett`, `belepett` FROM `tagok` WHERE 1";
+                using (MySqlDataReader dr = sql.ExecuteReader())
+                {
+                    while (dr.Read())
+                    {
+                        Tag tag = new Tag(dr.GetInt32("id"), dr.GetString("csaladnev"), dr.GetString("utonev"), dr.GetString("nem"), dr.GetDateTime("szuletett"), dr.GetDateTime("belepett"));
+                        tagok.Add(tag);
+                    }
+                }                
+                //-- befizetések beolvasása
+                sql.CommandText = "SELECT `id`, `datum`, `befizetes` FROM `befizetes` WHERE year(`datum`)=2021; ";
+                using (MySqlDataReader dr = sql.ExecuteReader())
+                {
+                    while (dr.Read())
+                    {
+                        Befizetes befizetes = new Befizetes(dr.GetDateTime("datum"), dr.GetInt32("befizetes"));
+                        int id = dr.GetInt32("id");
+                        tagok.Find(a => a.Id == id).befizetes.Add(befizetes);
+                    }
+                }
+            }
+            catch (MySqlException ex)
+            {
+                Console.WriteLine(ex.Message);
+                Console.ReadLine();
+                Environment.Exit(0);
+            }
+        }
+    }
+}
---
+++ ConsoleBookClub/BookClub_konzol/Tag.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookClub_konzol
+{
+    class Tag
+    {
+        public List<Befizetes> befizetes = new List<Befizetes>();
+        DateTime belepett;
+        string csaladnev;
+        int id;
+        string nem;
+        DateTime szuletett;
+        string utonev;
+
+        public Tag(int id, string csaladnev, string utonev, string nem, DateTime szuletett, DateTime belepett)
+        {
+            this.Id = id;
+            this.Csaladnev = csaladnev;
+            this.Utonev = utonev;
+            this.Nem = nem;
+            this.Szuletett = szuletett;
+            this.Belepett = belepett;
+        }
+
+        public string Nev { get => this.Csaladnev + " " + this.Utonev; }
+        public DateTime Belepett { get => belepett; set => belepett = value; }
+        public string Csaladnev { get => csaladnev; set => csaladnev = value; }
+        public int Id { get => id; set => id = value; }
+        public string Nem { get => nem; set => nem = value; }
+        public DateTime Szuletett { get => szuletett; set => szuletett = value; }
+        public string Utonev { get => utonev; set => utonev = value; }
+    }
+}
